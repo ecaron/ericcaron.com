3c69e89d7e5bde9e8ece55de3e63c199ec67d62d
Eric Caron <eric.caron@gmail.com>
2021-05-18 10:26:08 -0500

    Bumping to newest npm libraries & switching from semistandard to standard

--- src/_data/project.js
+++ src/_data/project.js
@@ -1,15 +1,15 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs')
+const path = require('path')
 
 module.exports = function () {
-  let data = {
+  const data = {
     environment: process.env.ELEVENTY_ENV
-  };
-  const rev = fs.readFileSync(path.join('.git', 'HEAD')).toString().trim();
-  if (rev.indexOf(':') === -1) {
-    data.buildNumber = rev;
-  } else {
-    data.buildNumber = fs.readFileSync(path.join('.git/', rev.substring(5))).toString();
   }
-  return data;
-};
+  const rev = fs.readFileSync(path.join('.git', 'HEAD')).toString().trim()
+  if (rev.indexOf(':') === -1) {
+    data.buildNumber = rev
+  } else {
+    data.buildNumber = fs.readFileSync(path.join('.git/', rev.substring(5))).toString()
+  }
+  return data
+}
